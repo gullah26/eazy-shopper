421aa1eb901a5b456dd330bb78f9d27cde00d406
Seun Ajetomobi <fullstackfory@gmail.com>
2022-07-26 19:18:45 +0000

    Product logic completed

--- eazyshopper/urls.py
+++ eazyshopper/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
-    # path('checkout/', include('checkout.urls')),
-    # path('profile/', include('profiles.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# handler404 = 'eazyshopper.views.handler404'
+handler404 = 'eazyshopper.views.handler404'
