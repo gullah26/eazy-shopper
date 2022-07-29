11e7cb6ef7214d0971277401eee7dc9f1c24b5a7
Seun Ajetomobi <fullstackfory@gmail.com>
2022-07-29 03:34:53 +0000

    Toast functionality was added

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +14,7 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
+     
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
