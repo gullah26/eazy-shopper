bf31529fff7e42373b46d94d582ac26e2f530bf5
Seun Ajetomobi <fullstackfory@gmail.com>
2022-08-09 13:11:55 +0000

    deployment stage 2

--- eazyshopper/settings.py
+++ eazyshopper/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eazyshopper.herokuapp.com', 'localhost']
 
 # CSRF_TRUSTED_ORIGINS = ['https://example.com']
 
@@ -139,12 +139,17 @@ WSGI_APPLICATION = 'eazyshopper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
