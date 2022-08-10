c37b2ab37703548b4854d01a73bd79687c055fa9
Seun Ajetomobi <fullstackfory@gmail.com>
2022-08-10 01:20:24 +0000

    sending confirmation email

--- cart/views.py
+++ cart/views.py
@@ -13,7 +13,7 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping cart """
+    """ Add the appropriate quantity of the specified product to the shopping cart """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
