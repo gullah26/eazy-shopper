a18700c3ec4c39577dec7b0e0a07fd8e5a3a3e92
Seun Ajetomobi <fullstackfory@gmail.com>
2022-08-18 02:27:11 +0000

    Add error folder and error testing

--- eazyshopper/urls.py
+++ eazyshopper/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +33,6 @@ urlpatterns = [
     path('shipping/', include('shipping.urls')),
     path('favorites/', include('favorite.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = 'eazyshopper.views.handler404'
